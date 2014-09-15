217e21aaf45434e44430906faa167cc5bda2f056
Gustavo Varallo <gustavo.varallo@live.com>
2014-09-15 14:37:26 -0300

    Corrected host URL on production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,5 +82,5 @@ Rails.application.configure do
   
   #Required for Heroku
   #Note to set this to your actual host
-  config.action_mailer.default_url_options = { host: => 'http://gusteresting.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: => 'gusteresting.herokuapp.com' }
 end
